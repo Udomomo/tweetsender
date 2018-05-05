7a412055ae5f4d9ba2783eff0b5dc741ae56bb50
Udomomo <batzuma@yahoo.co.jp>
2018-05-05 12:24:20 +0900

    TwitterのAPIが返すエラーメッセージを表示するように変更

--- tweet/tweet.py
+++ tweet/tweet.py
@@ -6,7 +6,10 @@ from subprocess import call
 
 def tweet():
 
-    TWEET_EDITOR = os.environ.get('TWEET_EDITOR','vim') 
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    TWEET_EDITOR = os.getenv('TWEET_EDITOR','vim') 
     initial_message = ["\n", "# Write your tweet above.\n", "# Lines starting with '#' will be ignored.\n"]
 
     with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
@@ -24,32 +27,37 @@ def tweet():
                 contents += decoded_line
             else:
                 break
-        print(contents)
 
     params = {"status": contents}
-    main(params)
+    if not params["status"]:
+        print("Aborted")
+        return
 
-def main(params):
+    send_tweet(params)
+
+def parse_error(res):
+    return res["errors"][0]['message']
+
+def send_tweet(params):
     url = "https://api.twitter.com/1.1/statuses/update.json"
     
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
+    # dotenv_path = join(dirname(__file__), '.env')
+    # load_dotenv(dotenv_path)
 
-    ck_env = os.environ.get('TWITTER_CK')
-    cs_env = os.environ.get('TWITTER_CS')
-    at_env = os.environ.get('TWITTER_AT')
-    as_env = os.environ.get('TWITTER_AS')
-
-    if not params["status"]:
-        return "Aborted"
+    ck_env = os.getenv('TWITTER_CK')
+    cs_env = os.getenv('TWITTER_CS')
+    at_env = os.getenv('TWITTER_AT')
+    as_env = os.getenv('TWITTER_AS')
 
     twitter = OAuth1Session(ck_env, cs_env, at_env, as_env)
     req = twitter.post(url, params)
-
+    
     if req.status_code == 200:
-        return "Tweet has sent"
+        print("Tweet has successfully sent")
     else:
-        return "Error: %d" % req.status_code
+        res = req.json()
+        err = parse_error(res)
+        print("Error: " + err)
 
 if __name__ == "__main__":
     print(tweet())
