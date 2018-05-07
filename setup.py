600371e1c3b743796b37893901233905d7a7f1b6
Udomomo <batzuma@yahoo.co.jp>
2018-05-07 23:58:10 +0900

    dotenvを使わずjsonファイルで設定を保存するよう変更

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        name='tweet',
+        name='tweetsender',
         version='0.0.1',
         packages=setuptools.find_packages(),
         entry_points={
             'console_scripts': [
-                'tweet = tweet.main:main'
+                'tweet = tweetsender.main:main'
+            ],
+            'tweet.command': [
+                'send = tweetsender.tweet:Tweet',
+                's = tweetsender.tweet:Tweet',
+                'config = tweetsender.tweet:Config'
             ]
         },
-        install_requires=['requests_oauthlib', 'python-dotenv'],
+        install_requires=['requests_oauthlib', 'python-dotenv', 'cliff'],
         author='Udomomo',
         author_email='batzuma@yahoo.co.jp',
         url='http://udomomo.hatenablog.com/'
